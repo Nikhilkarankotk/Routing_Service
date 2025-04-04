769f2b4edc9cbe0afe25f37af4abd20ff4e27da9
Nikhil13k <karankotnikhilkumar@gmail.com>
2025-04-04 13:01:20 +0530

    Initial Commit

---
+++ User/src/test/java/com/nkk/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkk.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
